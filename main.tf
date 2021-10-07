fe62c6f43be8911b3d0b726e362d6f8f8f0f12ad
AlphaBun <47685752+AlphaBun@users.noreply.github.com>
2021-10-07 09:31:19 +0800

    Add files via upload
    test

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 2.65"
     }
   }
-
   required_version = ">= 0.14.9"
 }
 
@@ -13,14 +12,175 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "RG0" {
-  name     = var.rg_name
-  location = "EastUS"
-}
 
-resource "azurerm_virtual_network" "vnet" {
-    name                = "VNet0"
-    address_space       = ["10.0.0.0/16"]
-    location            = "EastUS"
-    resource_group_name = azurerm_resource_group.RG0.name
+########################################### RG0 ##########################################
+#
+########################## RG #########################
+#resource "azurerm_resource_group" "RG0" {
+#  name     = var.RG0_name
+#  location = var.RG0_location
+#}
+#
+########################## VNet #########################
+#resource "azurerm_virtual_network" "VNet0" {
+#    name                = var.VNet0_name
+#    address_space       = [var.VNet0_address]
+#    location            = azurerm_resource_group.RG0.location
+#    resource_group_name = azurerm_resource_group.RG0.name
+#}
+#
+#resource "azurerm_subnet" "VNet0_JumpBoxSubnet" {
+#  name                 = var.VNet0_JumpBoxSubnet_name
+#  resource_group_name  = azurerm_resource_group.RG0.name
+#  virtual_network_name = azurerm_virtual_network.VNet0.name
+#  address_prefixes     = [var.VNet0_JumpBoxSubnet_address]
+#}
+#
+#resource "azurerm_subnet" "VNet0_AzureFirewallSubnet" {
+#  name                 = var.VNet0_AzureFirewallSubnet_name
+#  resource_group_name  = azurerm_resource_group.RG0.name
+#  virtual_network_name = azurerm_virtual_network.VNet0.name
+#  address_prefixes     = [var.VNet0_AzureFirewallSubnet_address]
+#}
+#
+########################## VM #########################
+#resource "azurerm_network_interface" "RG0_VM1nic" {
+#  name                = var.RG0_VM1nic_name
+#  location            = azurerm_virtual_network.VNet0.location
+#  resource_group_name = azurerm_resource_group.RG0.name
+#
+#  ip_configuration {
+#    name                          = "internal"
+#    subnet_id                     = azurerm_subnet.VNet0_JumpBoxSubnet.id
+#    private_ip_address_allocation = "Dynamic"
+#  }
+#}
+#
+#resource "azurerm_linux_virtual_machine" "RG0_VM1" {
+#  name                = var.RG0_VM1_name
+#  resource_group_name = azurerm_resource_group.RG0.name
+#  location            = azurerm_resource_group.RG0.location
+#  size                = "Standard_F2"
+#  admin_username      = "adminuser"
+#  admin_password      = "Hello123456!"
+#  network_interface_ids = [azurerm_network_interface.RG0_VM1nic.id]
+#
+#  disable_password_authentication = false # set to true if using SSH Keys
+#
+#  # This one is only applicable for azurerm_virtual_machine. Uncomment this line to delete the OS disk automatically when deleting the VM
+#  #delete_os_disk_on_termination = true
+#
+#  # This one is only applicable for azurerm_virtual_machine. Uncomment this line to delete the data disks automatically when deleting the VM
+#  #delete_data_disks_on_termination = true
+#
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
+#
+#  source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
+#}
+#
+#
+########################################### RG1 ##########################################
+#
+########################## RG #########################
+#resource "azurerm_resource_group" "RG1" {
+#  name     = var.RG1_name
+#  location = var.RG1_location
+#}
+#
+########################## VNet #########################
+#resource "azurerm_virtual_network" "VNet1" {
+#    name                = var.VNet1_name
+#    address_space       = [var.VNet1_address]
+#    location            = azurerm_resource_group.RG1.location
+#    resource_group_name = azurerm_resource_group.RG1.name
+#}
+#
+#resource "azurerm_subnet" "VNet1_Subnet1" {
+#  name                 = var.VNet1_Subnet1_name
+#  resource_group_name  = azurerm_resource_group.RG1.name
+#  virtual_network_name = azurerm_virtual_network.VNet1.name
+#  address_prefixes     = [var.VNet1_Subnet1_address]
+#}
+#
+#
+#
+#
+########################################### RG2 ##########################################
+#
+########################## RG #########################
+#resource "azurerm_resource_group" "RG2" {
+#  name     = var.RG2_name
+#  location = var.RG2_location
+#}
+#
+########################## VNet #########################
+#resource "azurerm_virtual_network" "VNet2" {
+#    name                = var.VNet2_name
+#    address_space       = [var.VNet2_address]
+#    location            = azurerm_resource_group.RG2.location
+#    resource_group_name = azurerm_resource_group.RG2.name
+#}
+#
+#resource "azurerm_subnet" "VNet2_Subnet1" {
+#  name                 = var.VNet2_Subnet1_name
+#  resource_group_name  = azurerm_resource_group.RG2.name
+#  virtual_network_name = azurerm_virtual_network.VNet2.name
+#  address_prefixes     = [var.VNet2_Subnet1_address]
+#}
+
+
+module "VNet3-FW1" {
+  source    = "./modules/VNet3-FW1"
+
+  ########################################## RG0 ##########################################
+
+  ######################### RG #########################
+  RG0_name = "RG0-EastUS-Prod-Hub"
+  RG0_location = "East US"
+
+  ######################### VNet #########################
+  VNet0_name = "VNet0-EastUS-Prod-Hub"
+  VNet0_address = "10.0.0.0/16"
+  VNet0_JumpBoxSubnet_name = "JumpBoxSubnet"
+  VNet0_JumpBoxSubnet_address = "10.0.0.0/24"
+  VNet0_AzureFirewallSubnet_name = "AzureFirewallSubnet"
+  VNet0_AzureFirewallSubnet_address = "10.0.1.0/24"
+
+  ######################### VM #########################
+  RG0_VM1nic_name = "RG0_VM1nic"
+  RG0_VM1_name = "RG0-VM1"
+
+
+  ########################################## RG1 ##########################################
+
+  ######################### RG #########################
+  RG1_name = "RG1-WestUS-Prod-Hub"
+  RG1_location = "West US"
+
+  ######################### VNet #########################
+  VNet1_name = "VNet1-WestUS-Prod-Hub"
+  VNet1_address = "10.1.0.0/16"
+  VNet1_Subnet1_name = "Subnet1"
+  VNet1_Subnet1_address = "10.1.0.0/24"
+
+
+  ########################################## RG2 ##########################################
+
+  ######################### RG #########################
+  RG2_name = "RG2-EastAsia-Prod-Hub"
+  RG2_location = "East Asia"
+
+  ######################### VNet #########################
+  VNet2_name = "VNet2-EastAsia-Prod-Hub"
+  VNet2_address = "10.2.0.0/16"
+  VNet2_Subnet1_name = "Subnet1"
+  VNet2_Subnet1_address = "10.2.0.0/24"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,85 @@
-variable "rg_name" {
-  default = "RG0"
+########################################## RG0 ##########################################
+
+######################### RG #########################
+variable "RG0_name" {
+  #default = "VNet0-EastUS-Prod-Hub"
+}
+variable "RG0_location" {
+  #default = "East US"
+}
+
+######################### VNet #########################
+variable "VNet0_name" {
+  #default = "VNet0-EastUS-Prod-Hub"
+}
+variable "VNet0_address" {
+  #default = "10.0.0.0/16"
+}
+variable "VNet0_JumpBoxSubnet_name" {
+  #default = "JumpBoxSubnet"
+}
+variable "VNet0_JumpBoxSubnet_address" {
+  #default = "10.0.0.0/24"
+}
+variable "VNet0_AzureFirewallSubnet_name" {
+  #default = "AzureFirewallSubnet"
+}
+variable "VNet0_AzureFirewallSubnet_address" {
+  #default = "10.0.1.0/24"
+}
+
+######################### VM #########################
+variable "RG0_VM1nic_name" {
+  #default = "RG0_VM1nic"
+}
+variable "RG0_VM1_name" {
+  #default = "RG0-VM1"
+}
+
+########################################## RG1 ##########################################
+
+######################### RG #########################
+variable "RG1_name" {
+  #default = "RG1-WestUS-Prod-Hub"
+}
+variable "RG1_location" {
+  #default = "West US"
+}
+
+######################### VNet #########################
+variable "VNet1_name" {
+  #default = "VNet1-WestUS-Prod-Hub"
+}
+variable "VNet1_address" {
+  #default = "10.1.0.0/16"
+}
+variable "VNet1_Subnet1_name" {
+  #default = "Subnet1"
+}
+variable "VNet1_Subnet1_address" {
+  #default = "10.1.0.0/24"
+}
+
+########################################## RG2 ##########################################
+
+######################### RG #########################
+variable "RG2_name" {
+  #default = "RG2-EastAsia-Prod-Hub"
+}
+variable "RG2_location" {
+  #default = "East Asia"
+}
+
+######################### VNet #########################
+variable "VNet2_name" {
+  #default = "VNet2-EastAsia-Prod-Hub"
+}
+variable "VNet2_address" {
+  #default = "10.2.0.0/16"
+}
+variable "VNet2_Subnet1_name" {
+  #default = "Subnet1"
+}
+variable "VNet2_Subnet1_address" {
+  #default = "10.2.0.0/24"
 }
